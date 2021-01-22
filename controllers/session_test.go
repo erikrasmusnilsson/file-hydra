5619af4b4e5f8f037108a8d680fb59ab228d9056
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-01-22 16:29:10 +0100

    Added more tests in session controller

--- controllers/session_test.go
+++ controllers/session_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -12,6 +13,12 @@ import (
 
 	"github.com/alicebob/miniredis"
 	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	testDir      = "test"
+	testFile     = "test.txt"
+	testFilePath = testDir + "/" + testFile
 )
 
 func TestIsValidFilename(t *testing.T) {
@@ -44,46 +51,70 @@ func request(body string, mux *httprouter.Router) *httptest.ResponseRecorder {
 	return rr
 }
 
-func TestCreateSessionNotFound(t *testing.T) {
-	os.Mkdir("test", 0755)
-	mux, mr := createMux("test")
+func TestCreateSessionOk(t *testing.T) {
+	os.Mkdir(testDir, 0755)
+	defer os.Remove(testDir)
+	os.Create(testFilePath)
+	defer os.Remove(testFilePath)
+
+	mux, mr := createMux(testDir)
 	defer mr.Close()
 
-	b := `{"filename":"test-file.txt", "expectedClients": 2}`
+	b := fmt.Sprintf(`{"filename": "%s", "expectedClients": %d}`, testFile, 2)
+	res := request(b, mux)
+
+	if code := res.Code; code != http.StatusCreated {
+		t.Errorf("Expected status code 201 created, got %d", code)
+	}
+}
+
+func TestCreateSessionNotFound(t *testing.T) {
+	os.Mkdir(testDir, 0755)
+	defer os.Remove(testDir)
+
+	mux, mr := createMux(testDir)
+	defer mr.Close()
+
+	b := fmt.Sprintf(`{"filename":"%s", "expectedClients": %d}`, testFile, 2)
 	res := request(b, mux)
 
 	if code := res.Code; code != http.StatusNotFound {
 		t.Errorf("Expected status code 404, got %d\n%s", code, res.Body)
 	}
-	os.Remove("test")
 }
 
 func TestCreateSessionEmptyFilename(t *testing.T) {
-	os.Mkdir("test", 0755)
+	os.Mkdir(testDir, 0755)
+	defer os.Remove(testDir)
+	os.Create(testFilePath)
+	defer os.Remove(testFilePath)
+
 	mux, mr := createMux("test")
 	defer mr.Close()
 
-	b := `{"filename":"", "expectedClients": 2}`
+	b := fmt.Sprintf(`{"filename":"", "expectedClients": %d}`, 2)
 	res := request(b, mux)
 
 	if code := res.Code; code != http.StatusBadRequest {
 		t.Errorf("Expected status code 400, got %d", code)
 	}
-	os.Remove("test")
 }
 
 func TestCreateSessionInvalidFilename(t *testing.T) {
-	os.Mkdir("test", 0755)
+	os.Mkdir(testDir, 0755)
+	defer os.Remove(testDir)
+	os.Create(testFilePath)
+	defer os.Remove(testFilePath)
+
 	mux, mr := createMux("test")
 	defer mr.Close()
 
-	b := `{"filename":"../main.go"}`
+	b := fmt.Sprintf(`{"filename":"../main.go", "expectedClients": %d}`, 2)
 	res := request(b, mux)
 
 	if code := res.Code; code != http.StatusBadRequest {
 		t.Errorf("Expected status code 400, got %d", code)
 	}
-	os.Remove("test")
 }
 
 func createMux(d string) (*httprouter.Router, *miniredis.Miniredis) {
